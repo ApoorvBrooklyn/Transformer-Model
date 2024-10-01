f026d16c9b90768caa77692ac70b2dacfb56f19c
ApoorvBrooklyn <apoorobot21@gmail.com>
2024-10-01 22:21:48 +0530

    Dataset and Training

--- Readme.md
+++ Readme.md
@@ -2,3 +2,4 @@
 - This MODEL is used to language translation
 - This model converts English to Italian and Vice-versa
 - Can be used to convert other languages too.
+- Dataset is taken from Hugging Face called opus_books
--- Transformer.py
+++ Transformer.py
@@ -156,7 +156,7 @@ class DecoderBlock(nn.Module):
         self.self_attention_block = self_attetion_block
         self.cross_attention_block = cross_attention_block
         self.feed_forward_block = feed_forward_block
-        self.residual_connections = nn.Module([ResidualConnection(dropout) for _ in range (3)])
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range (3)])
     
     def forward(self, x, encoder_output, src_mask, tgt_mask):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x, tgt_mask))
@@ -177,7 +177,7 @@ class Decoder(nn.Module):
     
 class ProjectionLayer(nn.Module):
     def __init__(self, d_model: int, vocab_size: int) -> None:
-        super.__init__()
+        super().__init__()
         self.proj = nn.Linear(d_model, vocab_size)
 
     def forward(self, x):
@@ -186,4 +186,71 @@ class ProjectionLayer(nn.Module):
     
 class Transformer(nn.Module):
     def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, tgt_embed:InputEmbeddings, src_pos: PostionalEncoding, tgt_pos: PostionalEncoding, projection_layer: ProjectionLayer) -> None:
-        
+        super().__init()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    def project(self, x):
+        return self.projection_layer(x)
+    
+
+def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len : int, d_model = 512, N: int = 6, h: int= 8, dropout :float =0.1, d_ff = 2048 ) -> Transformer:
+    # Create EMbedding layer
+    src_embed = InputEmbeddings(d_model, src_vocab_size)
+    tgt_embed = InputEmbeddings(d_model, tgt_vocab_size)
+
+    # Positional Encoding Layer
+    src_pos = PostionalEncoding(d_model, src_seq_len, dropout) # nO NEED TO MULTIPLE POSITIONAL ENCODING AS THEY DO THE SAME WORK
+    tgt_pos = PostionalEncoding(d_model, tgt_seq_len, dropout)
+
+    #CReate encoder block
+    encoder_blocks = []
+    for _ in range(N):
+        encoder_self_attention_block = MultiheadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        encoder_block = EncoderBlock(encoder_self_attention_block, feed_forward_block, dropout)
+        encoder_blocks.append(encoder_block)
+
+    # Decoder Block
+    decoder_blocks = []
+    for _ in range(N):
+        decoder_self_attention_block = MultiheadAttentionBlock(d_model, h, dropout)
+        decoder_cross_attention_block = MultiheadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        decoder_block = DecoderBlock(decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
+        decoder_blocks.append(decoder_block)
+
+
+    encoder = Encoder(nn.ModuleList(encoder_blocks))
+    decoder = Decoder(nn.ModuleList(decoder_blocks))
+
+    # CReate a projection Layer
+
+    projection_layer = ProjectionLayer(d_model, tgt_vocab_size)
+
+    transformer = Transformer(encoder, decoder, src_embed, tgt_embed, src_pos, tgt_pos, projection_layer)
+
+    # Initialize parameter
+
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+
+    return transformer
+
+
