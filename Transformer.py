b0091ea6a0f3f1a9334ffb01d064f35c911d08e9
ApoorvBrooklyn <apoorobot21@gmail.com>
2024-10-01 18:59:34 +0530

    Decoder_Block

--- Transformer.py
+++ Transformer.py
@@ -134,7 +134,7 @@ class EncoderBlock(nn.Module):
         self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
 
     def forward(self, x, src_mask):
-        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x, src_mask))
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x, src_mask))
         x = self.residual_connection[1](x, self.feed_forward_block)
         return x
 
@@ -149,3 +149,41 @@ class Encoder(nn.Module):
             x = layer(x, mask)
         return self.norm(x)
 
+class DecoderBlock(nn.Module):
+
+    def __init__(self, self_attetion_block: MultiheadAttentionBlock, cross_attention_block: MultiheadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attetion_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.Module([ResidualConnection(dropout) for _ in range (3)])
+    
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.self_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x,encoder_output, src_mask, tgt_mask )
+        return self.norm(x)
+    
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super.__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        #(batch, seq_len, d_model) -> (Batch, SeqLen, Vocab_size)
+        return torch.log_softmax(self.proj(x), dim = 1)
+    
+class Transformer(nn.Module):
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, tgt_embed:InputEmbeddings, src_pos: PostionalEncoding, tgt_pos: PostionalEncoding, projection_layer: ProjectionLayer) -> None:
+        
