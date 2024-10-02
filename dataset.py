bc86ee3662c6202d71f2ab77d295ffda3c6e7ba9
ApoorvBrooklyn <apoorobot21@gmail.com>
2024-10-02 20:13:06 +0530

    Few errors are present

--- dataset.py
+++ dataset.py
@@ -5,28 +5,33 @@ from typing import Any
 
 class BilingualDataset(Dataset):
 
-    def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
+    def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
         super().__init__()
         self.ds = ds
+        self.seq_len = seq_len
         self.tokenizer_src = tokenizer_src
         self.tokenizer_tgt = tokenizer_tgt
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
 
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['[SOS]'])], dtype= torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['[EOS]'])], dtype= torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['[PAD]'])], dtype= torch.int64)
+        self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_tgt.token_to_id("[PAD]")], dtype=torch.int64)
+
+        # self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['[SOS]'])], dtype= torch.int64)
+        # self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['[EOS]'])], dtype= torch.int64)
+        # self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['[PAD]'])], dtype= torch.int64)
 
     def __len__(self):
         return len(self.ds)
 
-    def __getitem__(self, index: Any) -> Any:
+    def __getitem__(self, index):
         src_target_pair = self.ds[index]
         src_text = src_target_pair['translation'][self.src_lang]
         tgt_text = src_target_pair['translation'][self.tgt_lang]
 
         enc_input_tokens = self.tokenizer_src.encode(src_text).ids
-        dec_input_tokens = self.tokenizer_src.encode(tgt_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
 
         # Padding words to uniform sequence length
         enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2 # MInus 2 is done so as we are subtracting Start of Sentence and End of Sentence 
@@ -35,34 +40,35 @@ class BilingualDataset(Dataset):
         if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0 :
             raise ValueError('Sentence is too long')
         
-        encoder_input = torch.concat(
+        encoder_input = torch.cat(
             [
                 self.sos_token, 
                 torch.tensor(enc_input_tokens, dtype = torch.int64),
                 self.eos_token,
                 torch.tensor([self.pad_token]* enc_num_padding_tokens, dtype= torch.int64)
-            ]
+            ],dim=0
         )
 
-        decoder_input = torch.concat(
+        decoder_input = torch.cat(
             [
                 self.sos_token, 
                 torch.tensor(dec_input_tokens, dtype = torch.int64),
                 torch.tensor([self.pad_token]* dec_num_padding_tokens, dtype= torch.int64)
-            ]
+            ],dim=0
         )
 
-        label = torch.concat(
+        label = torch.cat(
             [
                 torch.tensor(dec_input_tokens, dtype= torch.int64),
                 self.eos_token,
                 torch.tensor([self.pad_token]* dec_num_padding_tokens, dtype=torch.int64)
 
-            ]
+            ],dim=0
         )
 
         assert encoder_input.size(0) == self.seq_len
         assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
 
         return {
             "encoder_input" : encoder_input,
@@ -75,5 +81,5 @@ class BilingualDataset(Dataset):
         }
 
 def casual_mask(size):
-    mask = torch.triu(torch.ones(1,size, size), diagonal = 1).type(torch.int)
+    mask = torch.triu(torch.ones((1,size, size)), diagonal = 1).type(torch.int)
     return mask==0
